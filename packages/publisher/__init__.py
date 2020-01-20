e99d93d9c216500c8ddf9fd7773f1b36d20b677f
jingxuyang <jxy12301230>
2020-01-20 17:26:49 +0800

    添加logo

---
+++ packages/publisher/__init__.py
@@ -0,0 +1,12 @@
+# ==============================================================
+# -*- encoding: utf-8 -*-# 
+# !/usr/bin/env python
+# 
+#  @Version: 
+#  @Author: RunningMan
+#  @File: __init__.py.py
+#  @Create Time: 2020/1/14 17:57
+# ==============================================================
+
+import script
+reload(script)
---
+++ packages/publisher/script.py
@@ -0,0 +1,17 @@
+# ==============================================================
+# -*- encoding: utf-8 -*-# 
+# !/usr/bin/env python
+# 
+#  @Version: 
+#  @Author: RunningMan
+#  @File: run.py
+#  @Create Time: 2020/1/14 18:00
+# ==============================================================
+
+
+def aa():
+    print("aaaa")
+
+
+def bb():
+    print("bbbb")
