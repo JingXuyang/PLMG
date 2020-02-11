646ffeb7f528fbd80684e24d72c7c174ee3b5c9f
J_Struggle <943855569@qq.com>
2020-02-11 20:31:48 +0800

    添加描述

--- packages/maya/2016.5/scripts/userSetup.py
+++ packages/maya/2016.5/scripts/userSetup.py
@@ -1,11 +1,11 @@
 # ==============================================================
 # -*- encoding: utf-8 -*-# 
 # !/usr/bin/env python
-# 
-#  @Version: 
+#
 #  @Author: RunningMan
 #  @File: userSetup.py.py
 #  @Create Time: 2020/1/16 11:37
+#  @Description: 改脚本在maya启动的时候，自动运行。前提是把改脚本所在的路径添加到 PYTHON_PATH。
 # ==============================================================
 
 import maya.utils as utils
