68b2ffaad742c7945ba74d392447ca057501859f
Administrator <943855569@qq.cm>
2020-04-01 19:20:49 +0800

    增加获取镜头和资产任务的功能

--- main/dbif/__init__.py
+++ main/dbif/__init__.py
@@ -6,4 +6,8 @@
 # @File: __init__.py
 # @Create Time: 2020/2/9 0009 15:40
 
+import _cgteamwork
+reload(_cgteamwork)
 from _cgteamwork import CGT
+
+database = CGT()
--- main/dbif/_cgteamwork.py
+++ main/dbif/_cgteamwork.py
@@ -8,17 +8,22 @@
 # @Create Time: 2019/12/30 0030 20:41
 # ==============================================================
 
+import os
 import sys
 
 sys.path.append("C:/cgteamwork/bin/base")
+import engine
 import cgtw
 import cgtw2
 
+# ================================= Global variable =================================
+config_data = engine.getConfigs()
 
-# ------------------------------- Class -------------------------------
+
+# ================================= Class =================================
 class CGT(object):
 
-    def __init__(self, proj_name):
+    def __init__(self, proj_name=os.environ.get('XSYH_PROJECT')):
         self.tw = cgtw.tw()
         self.tw2 = cgtw2.tw()
         self.proj_name = proj_name
@@ -35,7 +40,7 @@ class CGT(object):
         except:
             return False
 
-    def getDataBase(self, proj_name=""):
+    def getDataBase(self, proj_name=os.environ.get('XSYH_PROJECT')):
         '''
         得到项目的数据库名称
         @param proj_name: 项目名称
@@ -73,7 +78,35 @@ class CGT(object):
         result = set()
         for i in asset_types:
             result.add(i["asset.type_name"])
-        return result
+
+        return list(result)
+
+    def getInfoId(self, seq, shot, model="shot"):
+        if model == "shot":
+            t_id_list = self.tw2.info.get_id(
+                self.database, model, [['shot.eps_name', '=', seq], 'and', ["shot.shot", "=", shot]]
+            )
+        else:
+            t_id_list = self.tw2.info.get_id(
+                self.database, "asset", [['asset.type_name', '=', seq], 'and', ["asset.asset_name", "=", shot]]
+            )
+        if t_id_list:
+            return t_id_list[0]
+
+    def getTaskId(self, seq='', shot='', step='', task='', model="shot"):
+        if model == "shot":
+            t_id_list = self.tw2.task.get_id(
+                self.database, model,
+                [['shot.eps_name', '=', seq], 'and', ["shot.shot", "=", shot], 'and', ["task.task_name", "=", task]]
+            )
+        else:
+            t_id_list = self.tw2.task.get_id(
+                self.database, "asset", [['asset.type_name', '=', seq], 'and', ["asset.asset_name", "=", shot], 'and',
+                                         ["task.task_name", "=", task], 'and', ["task.pipeline", "=", step]]
+            )
+
+        if t_id_list:
+            return t_id_list[0]
 
     def getAssets(self):
         '''
@@ -94,10 +127,44 @@ class CGT(object):
         if t_asset_list:
             for asset in t_asset_list:
                 result[asset["asset.type_name"]].append(asset["asset.asset_name"])
+
         return result
 
-    def getAssetTask(self, asset_type, asset_name):
-        pass
+    def getAssetTask(self, asset_type, asset_name, load_label=False):
+        '''
+        [{'asset.asset_name': u'charHei',
+          'asset.type_name': u'char',
+          'id': u'75A4620D-1CED-7013-1C86-CFD252F5E541',
+          'task.artist': u'\u674e\u5a67',},
+          ...
+          ]
+        '''
+        extend_filters = ['task.pipeline', 'task.task_name', 'asset.type_name', 'asset.asset_name', 'task.artist',
+                          'task.account']
+        filters = [i.get("sign") for i in config_data.get("global").get("asset_load")]
+        filters.extend(extend_filters)
+        t_id_list = self.tw2.task.get_id(
+            self.database, 'asset', [['asset.type_name', '=', asset_type], 'and', ["asset.asset_name", "=", asset_name]]
+        )
+        task_data = self.tw2.task.get(self.database, "asset", t_id_list, filters)
+
+        if not load_label:
+            return task_data
+
+        else:
+            new_task_data = task_data[:]
+            task_data = []
+            # task_dic: {'asset.asset': None, 'asset.cn_name': None, 'asset.type_name': u'char', ...}
+            for task_dic in new_task_data:
+                for sign in task_dic.keys():
+                    # asset_load: [{label: asset name, sign: "asset.asset_name", show: true}, {...}]
+                    asset_load = config_data.get("global").get("asset_load")
+                    for i in asset_load:
+                        if i.get("sign") == sign:
+                            task_dic[sign] = (task_dic.get(sign), i.get("label"))
+                task_data.append(task_dic)
+
+            return task_data
 
     def getSeqs(self):
         '''
@@ -106,6 +173,7 @@ class CGT(object):
         '''
         t_id_list = self.tw2.info.get_id(self.database, 'eps', [["eps.eps_name", "has", "%"]])
         t_data = self.tw2.info.get(self.database, "eps", t_id_list, ['eps.eps_name'])
+
         return sorted([i['eps.eps_name'] for i in t_data])
 
     def getShots(self, seq):
@@ -121,16 +189,35 @@ class CGT(object):
         else:
             return False
 
-    def getShotTask(self, seq, shot):
-        filter = ['task.pipeline', 'task.task_name', 'shot.eps_name', 'shot.shot', 'task.artist', 'task.account']
+    def getShotTask(self, seq, shot, load_label=False):
+        extend_filters = ['task.pipeline', 'task.task_name', 'shot.eps_name', 'shot.shot', 'task.artist',
+                          'task.account']
+        filters = [i.get("sign") for i in config_data.get("global").get("shot_load")]
+        filters.extend(extend_filters)
         t_id_list = self.tw2.task.get_id(
             self.database, 'shot', [['shot.eps_name', '=', seq], 'and', ["shot.shot", "=", shot]]
         )
-        task_data = self.tw2.task.get(self.database, "shot", t_id_list, filter)
-        return task_data
+        task_data = self.tw2.task.get(self.database, "shot", t_id_list, filters)
+
+        if not load_label:
+            return task_data
+
+        else:
+            new_task_data = task_data[:]
+            task_data = []
+            for task_dic in new_task_data:
+                for sign in task_dic.keys():
+                    shot_load = config_data.get("global").get("shot_load")
+                    for i in shot_load:
+                        if i.get("sign") == sign:
+                            task_dic[sign] = (task_dic.get(sign), i.get("label"))
+                task_data.append(task_dic)
+            return task_data
 
 
 if __name__ == '__main__':
     from pprint import pprint
+
     cgt = CGT("LongGong")
-    pprint(cgt.getShotTask("Seq001", "Shot010"))
+    # pprint(cgt.getShotTask("Seq001", "Shot010", load_label=True))
+    # pprint(cgt.getAssetTask("char", "charHei"))
