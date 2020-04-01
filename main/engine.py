07815893203a86307749025f1e7208a7d8f41225
Administrator <943855569@qq.cm>
2020-04-01 19:23:12 +0800

    无

--- main/engine.py
+++ main/engine.py
@@ -1,25 +1,81 @@
 # ==============================================================
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python
-# 
-#  @Version: 
+#
+#  @Version:
 #  @Author: RunningMan
 #  @File: engine.py
 #  @Create Time: 2020/2/4 19:51
 # ==============================================================
 
 import os
-
 import utilities
 
-# -------------------------- Global Values --------------------------
-CONFIG_DATA = utilities.load_yaml(os.path.join(os.environ.get('XSYH_ROOT_PATH'), "data/config.yaml"))
+# ================================= Global Values =================================
+CONFIG_PATH = os.path.join(os.environ.get('XSYH_ROOT_PATH'), "data/config.yaml")
+task_env = dict()
 
-# -------------------------- Functions --------------------------
-def main():
-    CONFIG_DATA = utilities.load_yaml(os.path.join(os.path.dirname(__file__), "../../data/config.yaml"))
-    print CONFIG_DATA
+
+# ================================= Functions =================================
+def getConfigs():
+    '''
+        得到 configs 的内容
+        @return:
+        '''
+    config_data = utilities.load_yaml(CONFIG_PATH)
+    return config_data
+
+
+def getTaskFromEnv(task_tree_item):
+    '''
+        得到当前的任务环境
+        @param task_tree_item:
+        @return:
+        '''
+    task_env = task_tree_item
+
+
+def relaodConfigs():
+    '''
+        重新读取 configs
+        @return:
+        '''
+    return getConfigs()
+
+
+def stepConfigs(step=""):
+    return getConfigs().get(step)
+
+
+def setEnv(dicts):
+    '''
+        设置环境变量
+        @param dicts: 环境变量字典
+        '''
+    for key, val in dicts.iteritems():
+        os.environ[key] = val
+
+
+# ================================= Class =================================
+class ActionEngine(object):
+    '''
+    此类用来获取 nodes.action中的子类
+    '''
+
+    def __init__(self):
+        pass
+
+
+class CheckEngine(object):
+    '''
+    此类用来获取 nodes.checking中的子类
+    '''
+    def __init__(self):
+        pass
+
+
 
 
 if __name__ == '__main__':
-    main()
+    # print getConfigs()
+    pass
